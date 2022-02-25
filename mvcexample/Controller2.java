e863d2c75ec6be2ab112b221658b5ad320265574
Gedyy <ged.100@outlook.com>
2022-02-25 01:41:26 +0000

    Add button to controller 2

--- mvcexample/Controller2.java
+++ mvcexample/Controller2.java
@@ -17,6 +17,7 @@ public class Controller2 extends JFrame
     private View3 view3;
     private View4 view4;
     private JButton clearViews;   // For direct message to views
+    private JButton incB;        // To prompt the model to "modify" itself (B component)
     private JButton refreshViews; // To prompt them to refresh their contents from the model
  
     // Constructor
@@ -37,6 +38,11 @@ public class Controller2 extends JFrame
         clearViews = new JButton("Clear views");
         window.add(clearViews);
         clearViews.addActionListener(this);
+        
+        incB = new JButton("Increment B");
+        window.add(incB);
+        incB.addActionListener(this);
+        
         refreshViews = new JButton("Refresh views");
         window.add(refreshViews);
         refreshViews.addActionListener(this);
@@ -57,10 +63,11 @@ public class Controller2 extends JFrame
         if (e.getSource() == clearViews) {
             view3.clear();
             view4.clear();
-        }
-        if (e.getSource() == refreshViews) {
+        } else if (e.getSource() == refreshViews) {
             view3.update();
             view4.update();
+        } else if (e.getSource() == incB) {
+        	model.modifyB();
         }
         
     } // actionPerformed
